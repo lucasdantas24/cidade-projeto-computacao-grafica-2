111ccfb640dcf378748300f28714885c943db6b3
rodrigo <rodrigohisa@gmail.com>
2023-11-05 19:14:45 -0300

    Feito alteracoes

--- examples/sampa/assets/lookat.frag
+++ examples/sampa/assets/lookat.frag
@@ -3,12 +3,13 @@
 precision mediump float;
 
 in vec4 fragColor;
+
 out vec4 outColor;
 
 void main() {
   if (gl_FrontFacing) {
     outColor = fragColor;
   } else {
-    outColor = vec4(fragColor.r * 0.5, 0, 0, fragColor.a);
+    outColor = fragColor * 0.5;
   }
 }
--- examples/sampa/window.hpp
+++ examples/sampa/window.hpp
@@ -2,33 +2,52 @@
 #define WINDOW_HPP_
 
 #include "abcgOpenGL.hpp"
-#include "model.hpp"
-#include "trackball.hpp"
+
+#include "camera.hpp"
+#include "ground.hpp"
+
+struct Vertex {
+  glm::vec3 position;
+
+  friend bool operator==(Vertex const &, Vertex const &) = default;
+};
 
 class Window : public abcg::OpenGLWindow {
 protected:
   void onEvent(SDL_Event const &event) override;
   void onCreate() override;
-  void onUpdate() override;
   void onPaint() override;
   void onPaintUI() override;
   void onResize(glm::ivec2 const &size) override;
   void onDestroy() override;
+  void onUpdate() override;
 
 private:
   glm::ivec2 m_viewportSize{};
 
-  Model m_model;
-  int m_trianglesToDraw{};
-
-  TrackBall m_trackBall;
-  float m_zoom{};
-
-  glm::mat4 m_modelMatrix{1.0f};
-  glm::mat4 m_viewMatrix{1.0f};
-  glm::mat4 m_projMatrix{1.0f};
-
+  GLuint m_VAO{};
+  GLuint m_VBO{};
+  GLuint m_EBO{};
   GLuint m_program{};
+
+  GLint m_viewMatrixLocation{};
+  GLint m_projMatrixLocation{};
+  GLint m_modelMatrixLocation{};
+  GLint m_colorLocation{};
+
+  Camera m_camera;
+  float m_dollySpeed{};
+  float m_truckSpeed{};
+  float m_panSpeed{};
+  int num_building;
+  Ground m_ground;
+
+  std::vector<glm::vec3> building_positions;
+  std::vector<Vertex> m_vertices;
+  std::vector<GLuint> m_indices;
+
+  std::vector<glm::vec3> generateRandomBuildingPositions(int numBuildings);
+  void loadModelFromFile(std::string_view path);
 };
 
 #endif
